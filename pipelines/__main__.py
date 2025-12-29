001cb29be70e6be3b80252d1e8e74ab10d00d380
Andrew Hall <andrewmartinhall2@gmail.com>
2025-12-29 13:44:52 -0700

    Fixed cron to be schedule

--- pipelines/__main__.py
+++ pipelines/__main__.py
@@ -2,4 +2,4 @@ from calendar_flow import calendar_backfill_flow
 from prefect.schedules import Cron
 
 if __name__ == "__main__":
-    calendar_backfill_flow.serve(cron=Cron("0 2 * * *", timezone="America/Denver"))
+    calendar_backfill_flow.serve(schedule=Cron("0 2 * * *", timezone="America/Denver"))
