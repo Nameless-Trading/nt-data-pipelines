b7def5444f5011e12b8dbc43efcaf41c5c53600e
andrewhall1124 <andrewmartinhall2@gmail.com>
2025-12-30 09:53:33 -0700

    Added etf prices flows.

--- pipelines/__main__.py
+++ pipelines/__main__.py
@@ -1,6 +1,7 @@
 from calendar_flow import calendar_backfill_flow
 from universe_flow import universe_backfill_flow
 from stock_prices_flow import stock_prices_backfill_flow, stock_prices_daily_flow
+from etf_prices_flow import etf_prices_backfill_flow, etf_prices_daily_flow
 from prefect import flow, serve
 from prefect.schedules import Cron
 
@@ -9,6 +10,7 @@ def daily_flow():
     calendar_backfill_flow()
     universe_backfill_flow()
     stock_prices_daily_flow()
+    etf_prices_daily_flow()
 
 if __name__ == "__main__":
     serve(
@@ -18,5 +20,6 @@ if __name__ == "__main__":
         ),
         calendar_backfill_flow.to_deployment(name="calendar-backfill-flow"),
         universe_backfill_flow.to_deployment(name="universe-backfill-flow"),
-        stock_prices_backfill_flow.to_deployment(name="stock-prices-backfill-flow")
+        stock_prices_backfill_flow.to_deployment(name="stock-prices-backfill-flow"),
+        etf_prices_backfill_flow.to_deployment(name="etf-prices-backfill-flow")
     )
---
+++ pipelines/etf_prices_flow.py
@@ -0,0 +1,135 @@
+from clients import get_alpaca_client, get_clickhouse_client
+from alpaca.data.requests import StockBarsRequest
+from alpaca.data.enums import Adjustment, DataFeed
+from alpaca.data.timeframe import TimeFrame, TimeFrameUnit
+import datetime as dt
+import polars as pl
+from prefect import flow, task
+from zoneinfo import ZoneInfo
+
+TICKERS = ["MTUM", "QUAL", "SIZE", "USMV", "VLUE"]
+
+@task
+def get_etf_prices(tickers: list[str], start: dt.datetime, end: dt.datetime) -> pl.DataFrame:
+    alpaca_client = get_alpaca_client()
+
+    stock_bars_request = StockBarsRequest(
+        symbol_or_symbols=tickers,
+        start=start,
+        end=end,
+        timeframe=TimeFrame(1, TimeFrameUnit.Day),
+        adjustment=Adjustment.ALL,
+        feed=DataFeed.IEX
+    )
+
+    stock_prices_raw = alpaca_client.get_stock_bars(stock_bars_request)
+
+    if not len(stock_prices_raw.df) > 0:
+        return pl.DataFrame(schema={
+            'ticker': pl.String,
+            'date': pl.String,
+            'open': pl.Float64,
+            'high': pl.Float64,
+            'low': pl.Float64,
+            'close': pl.Float64,
+            'volume': pl.Float64,
+            'trade_count': pl.Float64,
+            'vwap': pl.Float64
+        })
+    
+    stock_prices_clean = (
+        pl.from_pandas(stock_prices_raw.df.reset_index())
+        .select(
+            pl.col('symbol').alias('ticker'),
+            pl.col('timestamp').dt.convert_time_zone('America/Denver').dt.date().cast(pl.String).alias('date'),
+            'open',
+            'high',
+            'low',
+            'close',
+            'volume',
+            'trade_count',
+            'vwap'
+        )
+    )
+
+    return stock_prices_clean
+
+@task
+def get_etf_prices_batches(tickers: list[str], start: dt.datetime, end: dt.datetime) -> pl.DataFrame:
+    years = range(start.year, end.year + 1)
+    stock_prices_list = []
+    for year in years:
+        year_start = max(dt.datetime(year, 1, 1, tzinfo=start.tzinfo), start)
+        year_end = min(dt.datetime(year, 12, 31, tzinfo=end.tzinfo), end)
+
+        stock_prices = get_etf_prices(tickers, year_start, year_end)
+
+        stock_prices_list.append(stock_prices)
+
+    return (
+        pl.concat(stock_prices_list).sort('date', 'ticker')
+    )
+
+@task
+def upload_and_merge_etf_prices_df(stock_prices_df: pl.DataFrame):
+    clickhouse_client = get_clickhouse_client()
+    table_name = 'etf_prices'
+
+    # Create table if not exists
+    clickhouse_client.command(
+        f"""
+        CREATE TABLE IF NOT EXISTS {table_name} (
+            ticker String,
+            date String,
+            open Float64,
+            high Float64,
+            low Float64,
+            close Float64,
+            volume Float64,
+            trade_count Float64,
+            vwap Float64
+        )
+        ENGINE = ReplacingMergeTree()
+        ORDER BY (ticker, date)
+        """
+    )
+
+    # Insert into table
+    clickhouse_client.insert_df_arrow(table_name, stock_prices_df)
+
+    # Optimize table (deduplicate)
+    clickhouse_client.command(f"OPTIMIZE TABLE {table_name} FINAL")
+
+@flow
+def etf_prices_backfill_flow():
+    start = dt.datetime(2017, 1, 1, tzinfo=ZoneInfo("America/Denver"))
+    end = dt.datetime.today().replace(tzinfo=ZoneInfo("America/Denver")) - dt.timedelta(days=1)
+
+    etf_prices_df = get_etf_prices_batches(TICKERS, start, end)
+    upload_and_merge_etf_prices_df(etf_prices_df)
+
+@task
+def get_last_market_date() -> dt.date:
+    clickhouse_client = get_clickhouse_client()
+    last_market_date = clickhouse_client.query("SELECT MAX(date) FROM calendar")
+    return dt.datetime.strptime(last_market_date.result_rows[0][0], "%Y-%m-%d").date()
+
+@flow 
+def etf_prices_daily_flow():
+    last_market_date = get_last_market_date()
+    yesterday = (dt.datetime.today().replace(tzinfo=ZoneInfo("America/Denver")) - dt.timedelta(days=1)).date()
+
+    # Only get new data if yesterday was the last market date
+    if last_market_date != yesterday:
+        return
+
+    start = dt.datetime.combine(yesterday, dt.time(0, 0, 0)).replace(tzinfo=ZoneInfo("America/Denver"))
+    end = dt.datetime.combine(yesterday, dt.time(23, 59, 59)).replace(tzinfo=ZoneInfo("America/Denver"))
+
+    stock_prices_df = get_etf_prices_batches(TICKERS, start, end)
+
+    upload_and_merge_etf_prices_df(stock_prices_df)
+
+if __name__ == '__main__':
+    # etf_prices_backfill_flow()
+    etf_prices_daily_flow()
